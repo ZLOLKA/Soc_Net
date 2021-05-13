6ad1cc6991744f878b19836fcd5e901ee51005ad
Zlolka <jaroslavderkach@gmail.com>
2021-05-14 00:59:55 +0300

    Add environment variables to settings

--- Soc_Net/settings.py
+++ Soc_Net/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@4)d_=59p6m*_3x&5wgu*7t+ape0igv1yd8f8+xgmh^0l$=xx0'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", 0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -54,8 +55,7 @@ ROOT_URLCONF = 'Soc_Net.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Soc_Net.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE":   os.environ.get("SQL_ENGINE",   "django.db.backends.sqlite3"),
+        "NAME":     os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER":     os.environ.get("SQL_USER",     "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST":     os.environ.get("SQL_HOST",     "localhost"),
+        "PORT":     os.environ.get("SQL_PORT",     "5432"),
     }
 }
 
