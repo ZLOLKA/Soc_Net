fdc8d5f7690f94e5463d1b4819ebff0ab29e61ec
Zlolka <jaroslavderkach@gmail.com>
2021-05-14 19:54:30 +0300

    Add __str__ in models and registration models in admin

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 
 
@@ -11,6 +12,12 @@ class Person(models.Model):
     birth_day = models.IntegerField(null=True)
     mob_number = models.IntegerField(null=True)
 
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.patronymic}"
+
+
+admin.site.register(Person)
+
 
 class VK_acc(models.Model):
     city = models.CharField(max_length=30, null=True)
@@ -18,21 +25,51 @@ class VK_acc(models.Model):
     university = models.CharField(max_length=50, null=True)
     id_person = models.ForeignKey("Person", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.id}"
+
+
+admin.site.register(VK_acc)
+
 
 class VK_net(models.Model):
     who = models.ForeignKey("VK_acc", on_delete=models.CASCADE, related_name="+")
     on_whom = models.ForeignKey("VK_acc", on_delete=models.CASCADE, related_name="+")
 
+    def __str__(self):
+        return f"{self.who} on {self.on_whom}"
+
+
+admin.site.register(VK_net)
+
 
 class Instagram_acc(models.Model):
     id_person = models.ForeignKey("Person", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}"
+
+
+admin.site.register(Instagram_acc)
 
 
 class Instagram_net(models.Model):
     who = models.ForeignKey("Instagram_acc", on_delete=models.CASCADE, related_name="+")
     on_whom = models.ForeignKey("Instagram_acc", on_delete=models.CASCADE, related_name="+")
 
+    def __str__(self):
+        return f"{self.who} on {self.on_whom}"
+
+
+admin.site.register(Instagram_net)
+
 
 class VK_group(models.Model):
-    VK_accs = models.ManyToManyField(VK_acc)
-    name = models.CharField(max_length=20)
+    vk_accs = models.ManyToManyField(VK_acc, related_name="+")
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+admin.site.register(VK_group)
